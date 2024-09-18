03f512d3023b3f1db2a5659f4e6e55691fb51346
Sam McDonald <sam.peter.mcdonald@mahle.com>
2024-09-18 13:11:32 +0100

    Working out VScode No actual changes

--- File Movers/Inca File Mover/MOTAIncamover.py
+++ File Movers/Inca File Mover/MOTAIncamover.py
@@ -40,7 +40,7 @@ def job():
                     os.remove(dst_file)
                 shutil.copy2(src_file, dst_dir) #.copy for copy paste .move for cut paste
     print("Daily move done on " + now.strftime("%d-%m-%Y %H:%M:%S"))
-     
+
 print("Syncing files on program exercution")
 job()     
 schedule.every().day.at("05:10").do(job)
